76364b8bd0cfcb853f8c4b44944e4ec6207ea69d
Danilo Silva <danilo.silva@msn.com>
2025-04-13 11:46:04 -0300

    Adicionar projeto ReadTrack.Entities com classes Author e Book
    Foi criado um novo projeto chamado "ReadTrack.Entities" que contém as classes `Author` e `Book`. Essas classes utilizam anotações de dados para validação e mapeamento de colunas, definindo propriedades como `Id`, `Name`, `MainGenre`, `WritingStyle`, `Title`, `PageCount`, `Genre` e `AuthorId`. O arquivo de projeto `ReadTrack.Entities.csproj` foi configurado para usar o SDK do .NET 8.0, habilitando usings implícitos e suporte a tipos anuláveis.

---
+++ ReadTrack.Entities/Models/Author.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReadTrack.Entities.Models;
+
+public class Author
+{
+    [Column("AuthorId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O nome do autor é obrigatório.")]
+    [MaxLength(60, ErrorMessage = "O nome do autor pode ter no máximo 60 caracteres.")]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "O gênero principal é obrigatório.")]
+    [MaxLength(60, ErrorMessage = "O gênero principal pode ter no máximo 60 caracteres.")]
+    public string? MainGenre { get; set; }
+
+    public string? WritingStyle { get; set; }
+
+    public ICollection<Book>? Books { get; set; }
+}
---
+++ ReadTrack.Entities/Models/Book.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReadTrack.Entities.Models;
+
+public class Book
+{
+    [Column("BookId")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O título do livro é obrigatório.")]
+    [MaxLength(30, ErrorMessage = "O título pode ter no máximo 30 caracteres.")]
+    public string? Title { get; set; }
+
+    [Required(ErrorMessage = "O número de páginas é obrigatório.")]
+    public int PageCount { get; set; }
+
+    [Required(ErrorMessage = "O gênero é obrigatório.")]
+    [MaxLength(20, ErrorMessage = "O gênero pode ter no máximo 20 caracteres.")]
+    public string? Genre { get; set; }
+
+    [ForeignKey(nameof(Author))]
+    public Guid AuthorId { get; set; }
+
+    public Author? Author { get; set; }
+}
