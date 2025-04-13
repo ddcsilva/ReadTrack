39d7b92d2bfacd2ce1aa2059dc35352f21092f1e
Danilo Silva <danilo.silva@msn.com>
2025-04-13 11:11:34 -0300

    Refatoração do sistema de logging
    Removida referência à pasta de logs no `ReadTrack.Api.csproj`.
    Alterada a implementação da classe `LoggerManager` para usar uma instância estática de `ILogger`.
    Atualizada a configuração do NLog em `nlog.config` para um novo formato de arquivo de log e nível de log "Debug".
    Entradas de log em `internallog.txt` agora refletem as novas configurações.

--- ReadTrack.LoggerService/LoggerManager.cs
+++ ReadTrack.LoggerService/LoggerManager.cs
@@ -5,10 +5,12 @@ namespace ReadTrack.LoggerService;
 
 public class LoggerManager : ILoggerManager
 {
-    private static readonly ILogger _logger = LogManager.GetCurrentClassLogger();
-
-    public void LogDebug(string message) => _logger.Debug(message);
-    public void LogError(string message) => _logger.Error(message);
-    public void LogInfo(string message) => _logger.Info(message);
-    public void LogWarn(string message) => _logger.Warn(message);
+    private static ILogger logger = LogManager.GetCurrentClassLogger();
+    public LoggerManager()
+    {
+    }
+    public void LogDebug(string message) => logger.Debug(message);
+    public void LogError(string message) => logger.Error(message);
+    public void LogInfo(string message) => logger.Info(message);
+    public void LogWarn(string message) => logger.Warn(message);
 }
