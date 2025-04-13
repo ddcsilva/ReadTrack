12fce438a3c7a1c6a3591b32b4a5d4a901006892
Danilo Silva <danilo.silva@msn.com>
2025-04-13 11:58:40 -0300

    Adicionar projeto ReadTrack.Repository e configuração DB
    Este commit inclui a adição do projeto `ReadTrack.Repository`, referenciado no `ReadTrack.Api.csproj`. Foram implementadas configurações de conexão ao banco de dados SQL Server no `appsettings.json` para o banco `ReadTrackDb`. O projeto foi configurado com o SDK do .NET e inclui referências ao Entity Framework Core. Também foram criadas as classes `RepositoryContextFactory` e `RepositoryContext`, que gerenciam o contexto do banco de dados e definem as entidades `Author` e `Book`.

---
+++ ReadTrack.Repository/RepositoryContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReadTrack.Entities.Models;
+
+namespace ReadTrack.Repository;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Author>? Authors { get; set; }
+    public DbSet<Book>? Books { get; set; }
+}
