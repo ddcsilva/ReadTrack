97262436406aa8ade923bb0a0fd114ac4f610442
Danilo Silva <danilo.silva@msn.com>
2025-04-13 14:47:44 -0300

    Adicionar configuração do contexto SQL e namespaces EF Core
    Implementado o método `ConfigureSqlContext` no arquivo `ServiceExtensions.cs` para configurar o contexto do banco de dados usando SQL Server. Adicionados namespaces do Entity Framework Core. O método foi integrado na configuração de serviços no `Program.cs`, garantindo que o contexto do banco de dados seja corretamente configurado ao iniciar a aplicação.

--- ReadTrack.Api/Extensions/ServiceExtensions.cs
+++ ReadTrack.Api/Extensions/ServiceExtensions.cs
@@ -1,4 +1,5 @@
-﻿using ReadTrack.Contracts;
+﻿using Microsoft.EntityFrameworkCore;
+using ReadTrack.Contracts;
 using ReadTrack.LoggerService;
 using ReadTrack.Repository;
 using ReadTrack.Service;
@@ -30,4 +31,7 @@ public static class ServiceExtensions
     public static void ConfigureLoggerService(this IServiceCollection services) => services.AddSingleton<ILoggerManager, LoggerManager>();
     public static void ConfigureRepositoryManager(this IServiceCollection services) => services.AddScoped<IRepositoryManager, RepositoryManager>();
     public static void ConfigureServiceManager(this IServiceCollection services) => services.AddScoped<IServiceManager, ServiceManager>();
+    public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+        services.AddDbContext<RepositoryContext>(opts =>
+            opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
 }
